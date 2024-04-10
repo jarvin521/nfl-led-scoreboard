84bd1027b9c34aee3ad37bddaeb28dc5277fe29b
Jason Arvin <jarvin521@hotmail.com>
2024-04-10 14:09:28 -0500

    logos

--- ncaam.ps1
+++ ncaam.ps1
@@ -1,6 +1,9 @@
+$index = 1
 $webrequest = invoke-webrequest -Uri "https://www.espn.com/mens-college-basketball/teams"
+$teamhash = @{}
 while($index -lt 725){$string = ($webrequest.RawContent -split ('"u":"/mens-college-basketball/team/_/id/') -split('"n":"'))[$index]
  $id = [regex]::Match($string, '"id":"(\d+)"').Groups[1].Value
- $id >> ncaam.txt
+ $teamName = $string.split('","')[0]
+ $teamHash[$teamName] = $id
  $index = $index + 2
 }
